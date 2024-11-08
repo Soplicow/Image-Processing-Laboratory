e163ee0803133cf5d92e37f15f1c9654f58c630a
Soplicow <wojtekk2307@gmail.com>
2024-11-08 15:47:51 +0100

    Comments in measuring and noise .cpp

--- image-processing-library/src/Measuring.cpp
+++ image-processing-library/src/Measuring.cpp
@@ -3,6 +3,16 @@
 
 using namespace cimg_library;
 
+/**
+ * @brief Computes the Signal-to-Noise Ratio (SNR) between an original image and a modified image.
+ *
+ * The SNR is calculated using the formula:
+ * SNR = 10 * log10(sum(originalImage^2) / sum((originalImage - modifiedImage)^2))
+ *
+ * @param originalImage The original image.
+ * @param modifiedImage The modified image.
+ * @return The Signal-to-Noise Ratio (SNR) in decibels (dB).
+ */
 float Measuring::signalToNoiseRatio(CImg<unsigned char> originalImage, CImg<unsigned char> modifiedImage) {
     float snr = 0.0;
     float originalSum = 0.0;
@@ -23,6 +33,22 @@ float Measuring::signalToNoiseRatio(CImg<unsigned char> originalImage, CImg<unsi
     return snr;
 }
 
+/**
+ * @brief Calculates the Peak Signal-to-Noise Ratio (PSNR) between two images.
+ *
+ * The PSNR is a measure of the quality of the modified image compared to the original image.
+ * It is expressed in decibels (dB) and is calculated using the formula:
+ * 
+ * \f[
+ * \text{PSNR} = 20 \cdot \log_{10}(255) - 10 \cdot \log_{10}(\text{MSE})
+ * \f]
+ * 
+ * where MSE is the Mean Square Error between the original and modified images.
+ *
+ * @param originalImage The original image.
+ * @param modifiedImage The modified image.
+ * @return The PSNR value in decibels (dB).
+ */
 float Measuring::peakSignalToNoiseRatio(CImg<unsigned char> originalImage, CImg<unsigned char> modifiedImage) {
     float psnr = 0.0;
     
@@ -30,6 +56,17 @@ float Measuring::peakSignalToNoiseRatio(CImg<unsigned char> originalImage, CImg<
     return psnr;
 }
 
+/**
+ * @brief Computes the Mean Square Error (MSE) between two images.
+ *
+ * This function calculates the Mean Square Error (MSE) between the original image
+ * and the modified image. The MSE is a measure of the average squared difference
+ * between corresponding pixels of the two images.
+ *
+ * @param originalImage The original image.
+ * @param modifiedImage The modified image to compare against the original image.
+ * @return The Mean Square Error (MSE) value.
+ */
 float Measuring::meanSquareError(CImg<unsigned char> originalImage, CImg<unsigned char> modifiedImage) {
     float mse = 0.0;
     cimg_forXYZC(originalImage, x, y, z, c) {
@@ -39,12 +76,32 @@ float Measuring::meanSquareError(CImg<unsigned char> originalImage, CImg<unsigne
     return mse;
 }
 
+/**
+ * @brief Calculates the Peak Mean Square Error (PMSE) between the original and modified images.
+ *
+ * The PMSE is a normalized version of the Mean Square Error (MSE) that divides the MSE by the square of the maximum possible pixel value (255 for 8-bit images).
+ *
+ * @param originalImage The original image to compare.
+ * @param modifiedImage The modified image to compare.
+ * @return The Peak Mean Square Error (PMSE) value.
+ */
 float Measuring::peakMeanSquareError(CImg<unsigned char> originalImage, CImg<unsigned char> modifiedImage) {
     float pmse = 0.0;
     pmse = meanSquareError(originalImage, modifiedImage) / pow(255, 2);
     return pmse;
 }
 
+/**
+ * @brief Computes the maximum difference between corresponding pixels of two images.
+ *
+ * This function iterates over each pixel in the original and modified images,
+ * calculates the absolute difference between the corresponding pixels, and
+ * returns the maximum difference found.
+ *
+ * @param originalImage The original image.
+ * @param modifiedImage The modified image.
+ * @return The maximum absolute difference between corresponding pixels of the two images.
+ */
 float Measuring::maximumDifference(CImg<unsigned char> originalImage, CImg<unsigned char> modifiedImage) {
     int maxDiff = 0;
     cimg_forXYZC(originalImage, x, y, z, c) {
--- image-processing-library/src/Noise.cpp
+++ image-processing-library/src/Noise.cpp
@@ -6,18 +6,29 @@
 
 using namespace cimg_library;
 
+/**
+ * Applies a minimum filter to the given image.
+ *
+ * The minimum filter replaces each pixel value with the minimum value
+ * within a specified window size centered around the pixel.
+ *
+ * @param image The input image to be filtered.
+ * @param windowSize The size of the window to be used for the filter. Must be an odd number.
+ * @return A new image with the minimum filter applied.
+ * @throws std::invalid_argument if the window size is not an odd number.
+ */
 CImg<unsigned char> Noise::minFilter(CImg<unsigned char> image, int windowSize) {
     if (windowSize % 2 == 0) {
         throw std::invalid_argument("Window size must be odd");
     }
 
     CImg<unsigned char> filteredImage = image;
+    int halfWindowSize = windowSize / 2;
+    int windowArea = windowSize * windowSize;
 
     for (int c = 0; c < image.spectrum(); ++c) { // Iterate over each color channel
-        for (int x = windowSize / 2; x < image.width() - windowSize / 2; ++x) {
-            for (int y = windowSize / 2; y < image.height() - windowSize / 2; ++y) {
-                int halfWindowSize = windowSize / 2;
-                int windowArea = windowSize * windowSize;
+        for (int x = halfWindowSize; x < image.width() - halfWindowSize; ++x) {
+            for (int y = halfWindowSize; y < image.height() - halfWindowSize; ++y) {
                 int* window = new int[windowArea];
                 int windowIndex = 0;
 
@@ -33,23 +44,36 @@ CImg<unsigned char> Noise::minFilter(CImg<unsigned char> image, int windowSize) 
                 delete[] window;
             }
         }
-    }
+    }    
 
     return filteredImage;
 }
 
+/**
+ * Applies a maximum filter to the given image.
+ *
+ * The maximum filter replaces each pixel value with the maximum value
+ * within a specified window size centered around the pixel.
+ *
+ * @param image The input image to be filtered.
+ * @param windowSize The size of the window to be used for the maximum filter.
+ *                   Must be an odd number.
+ * @return A new image with the maximum filter applied.
+ * @throws std::invalid_argument if the window size is not an odd number.
+ */
 CImg<unsigned char> Noise::maxFilter(CImg<unsigned char> image, int windowSize) {
     if (windowSize % 2 == 0) {
         throw std::invalid_argument("Window size must be odd");
     }
 
     CImg<unsigned char> filteredImage = image;
+    int halfWindowSize = windowSize / 2;
+    int windowArea = windowSize * windowSize;
 
     for (int c = 0; c < image.spectrum(); ++c) { // Iterate over each color channel
-        for (int x = windowSize / 2; x < image.width() - windowSize / 2; ++x) {
-            for (int y = windowSize / 2; y < image.height() - windowSize / 2; ++y) {
-                int halfWindowSize = windowSize / 2;
-                int windowArea = windowSize * windowSize;
+        for (int x = halfWindowSize; x < image.width() - halfWindowSize; ++x) {
+            for (int y = halfWindowSize; y < image.height() - halfWindowSize; ++y) {
+                
                 int* window = new int[windowArea];
                 int windowIndex = 0;
 
@@ -70,9 +94,27 @@ CImg<unsigned char> Noise::maxFilter(CImg<unsigned char> image, int windowSize) 
     return filteredImage;
 }
 
+/**
+ * Applies an adaptive median filter to the given image.
+ *
+ * The adaptive median filter is used to remove noise from an image while preserving edges.
+ * It works by iteratively increasing the window size until a suitable median value is found
+ * or the maximum window size is reached.
+ *
+ * @param image The input image to be filtered.
+ * @param windowSize The initial window size for the filter. Must be an odd number.
+ * @param maxWindowSize The maximum window size for the filter. Must be an odd number.
+ * @return The filtered image.
+ * @throws std::invalid_argument if the initial window size is not odd.
+ * @throws std::invalid_argument if the maximum window size is not odd.
+ */
 CImg<unsigned char> Noise::adaptiveMedianFilter(CImg<unsigned char> image, int windowSize, int maxWindowSize) {
     if (windowSize % 2 == 0) {
         throw std::invalid_argument("Window size must be odd");
+    }
+
+    if (maxWindowSize % 2 == 0) {
+        throw std::invalid_argument("Max window size must be odd");
     }
 
     CImg<unsigned char> filteredImage = image;
